e9f41e0fccc1ac87c03b130d03e9af9af3bef939
NateLee <intoqasa@gmail.com>
2018-08-31 09:15:21 +0800

    Initial commit.

---
+++ src/Client.java
@@ -0,0 +1,33 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    // initialize socket and input output streams
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    // constructor to put ip address and port
+    public Client(String ipAddress, int port) {
+
+        // Establish a connection
+        try {
+            socket = new Socket(ipAddress, port);
+            System.out.println("Connected!");
+
+            // Takes input from terminal input
+            input = new DataInputStream(System.in);
+
+            // Sends output to the socket
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
